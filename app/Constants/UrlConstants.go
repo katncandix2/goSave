f0a2e014598e510c6c5a3559ad0c8221fc8e46cb
guruiqin <254971939@qq.com>
2018-06-22 20:06:16 +0800

    '新增'

---
+++ app/Constants/UrlConstants.go
@@ -0,0 +1,13 @@
+/*
+@Time : 2018/6/22 15:42 
+@Author : lenovo
+@File : UrlConstants
+@Software: GoLand
+*/
+package Constants
+
+const (
+
+	USER_ORDER_BY_USERID_ARR = "http://orderapi.xesv5.com/Order/Order/getOrderInfosByStuId"
+
+)
